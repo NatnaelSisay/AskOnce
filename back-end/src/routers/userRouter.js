ac042d22ab461e22f8e610ad175a30ef9d570607
Tinsae Yared <tinsae.yb@gmail.com>
2023-03-17 15:18:47 -0500

    is student role

--- back-end/src/routers/userRouter.js
+++ back-end/src/routers/userRouter.js
@@ -3,13 +3,11 @@ const {
   loginController,
   signupController,
 } = require("../controllers/userController");
-const { roleIsProfessor } = require("../middlewares/isRoleProfessor");
 const { logginMiddleware } = require("../middlewares/loginMiddleware");
 const { signupMiddleware } = require("../middlewares/signupMiddleware");
 const UserRouter = new express.Router();
 
 UserRouter.post("/login", logginMiddleware, loginController);
 UserRouter.post("/signup", signupMiddleware, signupController);
-UserRouter.post("/", roleIsProfessor, (req, res) => res.json());
 
 module.exports = UserRouter;
