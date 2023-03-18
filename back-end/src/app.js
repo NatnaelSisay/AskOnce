eb224605c2c8135affa89741bac6a0d8b6ca1849
NatnaelSisay <natyjava8@gmail.com>
2023-03-18 15:09:48 -0500

    renamed files and add questions router link

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -12,8 +12,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/users", UserRouter);
 app.use("/class-room", ClassRoomRouter);
+app.use("/questions", questionRouter);
+
 app.use("*", (err, req, res, next) => {
   if (err instanceof ResponseError) {
     res.statusCode = err.statusCode;
