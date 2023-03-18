8258893d4c15366ef8064d978e1fddfb768abba2
Tinsae Yared <tinsae.yb@gmail.com>
2023-03-17 22:12:05 -0500

    changed class room model

--- back-end/src/models/classRoomModel.js
+++ back-end/src/models/classRoomModel.js
@@ -1,15 +1,63 @@
 const mongoose = require("mongoose");
+const { connectDb } = require("../dbConnect");
 const { userSchema } = require("./userModel");
+connectDb();
 
 const classRoomSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  professor: userSchema,
-  students: [userSchema],
+  professor: {
+    _id: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    role: {
+      type: String,
+      required: true,
+    },
+  },
+  students: [
+    {
+      _id: {
+        type: String,
+        required: true,
+      },
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+
+      role: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
-
-
 
 module.exports = mongoose.model("ClassRoom", classRoomSchema);
