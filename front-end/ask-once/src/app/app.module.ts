edc4841aed87692654c9af78a8ee11ad451f8e53
Tinsae Yared <tinsae.yb@gmail.com>
2023-03-19 13:45:46 -0500

    added auth guard for the app

--- front-end/ask-once/src/app/app.module.ts
+++ front-end/ask-once/src/app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, inject, Inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import readTokenFromStorage from './utils/readTokenFromStorage';
+import { Router } from '@angular/router';
 const getBaseUrl = () => 'http://localhost:3000';
 
 @NgModule({
@@ -15,7 +17,18 @@ const getBaseUrl = () => 'http://localhost:3000';
     AppRoutingModule,
     HttpClientModule,
   ],
-  providers: [{ provide: 'BASE_URL', useFactory: getBaseUrl }],
+  providers: [
+    { provide: 'BASE_URL', useFactory: getBaseUrl },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (router = inject(Router)) => {
+        const token = readTokenFromStorage();
+        if (token === null) {
+          router.navigateByUrl('/auth');
+        }
+      },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
