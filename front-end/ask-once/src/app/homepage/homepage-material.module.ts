a7fb2e2f880f879e99da2903273bf1cd405c5cec
NatnaelSisay <natyjava8@gmail.com>
2023-03-19 16:54:39 -0500

    added positionning for ui elements

--- front-end/ask-once/src/app/homepage/homepage-material.module.ts
+++ front-end/ask-once/src/app/homepage/homepage-material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
@@ -12,6 +13,7 @@ const modules = [
   MatButtonModule,
   MatCardModule,
   MatDividerModule,
+  MatGridListModule,
 ];
 
 @NgModule({
