def65b91af9f532d19629ce79d874bfb1a49ed48
Tinsae Yared <tinsae.yb@gmail.com>
2023-03-20 10:53:06 -0500

    class room creation done

--- front-end/ask-once/src/app/homepage/homepage-material.module.ts
+++ front-end/ask-once/src/app/homepage/homepage-material.module.ts
@@ -5,6 +5,11 @@ import { MatButtonModule } from '@angular/material/button';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatListModule } from '@angular/material/list';
 
 const modules = [
   MatToolbarModule,
@@ -12,6 +17,11 @@ const modules = [
   MatButtonModule,
   MatCardModule,
   MatDividerModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatAutocompleteModule,
+  MatListModule,
 ];
 
 @NgModule({
