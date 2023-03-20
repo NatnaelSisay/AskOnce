1ec0cb6237bc0bde1ec8ab62bca52ae37e6720cc
Tinsae Yared <tinsae.yb@gmail.com>
2023-03-20 10:52:34 -0500

    create class room service

---
+++ front-end/ask-once/src/app/homepage/homepage/createClassRoom/create-class-room.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { catchError, retry, throwError } from 'rxjs';
+import IUser from 'src/app/interface/IUser';
+
+@Injectable({
+  providedIn: 'any',
+})
+export class CreateClassRoomService {
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') public baseUrl: string
+  ) {}
+  search(value: string) {
+    return this.http
+      .get<IUser[]>(`${this.baseUrl}/users?search=${value}`)
+      .pipe(retry(3), catchError(this.handleError));
+  }
+
+  createClassRoom(name: string, users: IUser[]) {
+    return this.http
+      .post<IUser[]>(`${this.baseUrl}/class-room/`, {
+        name,
+        students: users ?? [],
+      })
+      .pipe(retry(3), catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.log(error);
+
+    return throwError(() => [
+      'Something bad happened; please try again later.',
+    ]);
+  }
+}
