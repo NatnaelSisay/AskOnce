77e8b2893c8b8d8156fda14e8135f98dc93c9c80
Tinsae Yared <tinsae.yb@gmail.com>
2023-03-20 21:04:37 -0500

    redirect user to login screen if 401 is reponded

--- front-end/ask-once/src/app/app-guard.interceptor.ts
+++ front-end/ask-once/src/app/app-guard.interceptor.ts
@@ -5,25 +5,35 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { AuthService } from './auth/auth.service';
 import readTokenFromStorage from './utils/readTokenFromStorage';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AppGuardInterceptor implements HttpInterceptor {
   authService = inject(AuthService);
-  constructor() {}
+  constructor(private router: Router) {}
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
     const token = readTokenFromStorage();
-    if (token) {
-      const headerClone = request.clone({
-        headers: request.headers.set('Authorization', 'Bearer ' + token),
-      });
-      return next.handle(headerClone);
-    } else return next.handle(request);
+
+    const headerClone = request.clone({
+      headers: request.headers.set('Authorization', 'Bearer ' + token),
+    });
+    return next.handle(headerClone).pipe(
+      tap({
+        next: (event) => {},
+        error: (error) => {
+          if (error.status === 401) {
+            this.authService.logout();
+            this.router.navigateByUrl('/auth');
+          }
+        },
+      })
+    );
   }
 }
